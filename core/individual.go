265183a3f9ff8610f60b74c68551a958db3c5be4
mick-roper <mick.roper@dazn.com>
2018-08-22 08:44:20 +0100

    added arg to NewIndividual

--- core/individual.go
+++ core/individual.go
@@ -5,26 +5,30 @@ import (
 	"time"
 )
 
-// GeneCount indicates the number of genes in each individual
-const GeneCount = 5
-
 // Individual that contains genes
 type Individual struct {
-	fitness int
+	Fitness int
 	genes   []int
 }
 
 // NewIndividual creates a new individual
-func NewIndividual() *Individual {
-	genes := make([]int, GeneCount)
+func NewIndividual(geneLength int) *Individual {
+	genes := make([]int, geneLength)
 
 	src := rand.NewSource(time.Now().UnixNano())
 	x := rand.New(src).Int()
 
-	for i := 0; i < GeneCount; i++ {
+	for i := 0; i < len(genes); i++ {
 		genes[i] = x & 0x01
 		x >>= 1
 	}
 
 	return &Individual{0, genes}
+}
+
+// CalcFitness calculates the fitness of the individual
+func (i *Individual) CalcFitness() {
+	for n := 0; n < len(i.genes); n++ {
+
+	}
 }
--- core/individual_test.go
+++ core/individual_test.go
@@ -5,14 +5,20 @@ import (
 )
 
 func Test_NewIndividual(t *testing.T) {
-	i := NewIndividual()
+	const l = 10
+	i := NewIndividual(l)
 
 	if i == nil {
 		t.Error("individual is nil")
 		return
 	}
 
-	if len(i.genes) != GeneCount {
+	if i.Fitness != 0 {
+		t.Error("fitness should be 0")
+		return
+	}
+
+	if len(i.genes) != l {
 		t.Error("genes array length is wrong")
 	}
 }
