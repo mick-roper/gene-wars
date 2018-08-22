5bbab72fcca6500ee49ae6a673ccf5ad5477ad02
mick-roper <mick.roper@dazn.com>
2018-08-22 08:40:27 +0100

    extended NewIndivdual function

--- core/individual.go
+++ core/individual.go
@@ -1,18 +1,30 @@
 package core
 
+import (
+	"math/rand"
+	"time"
+)
+
 // GeneCount indicates the number of genes in each individual
 const GeneCount = 5
 
 // Individual that contains genes
 type Individual struct {
-	fitness float64
-	genes   []float64
+	fitness int
+	genes   []int
 }
 
 // NewIndividual creates a new individual
 func NewIndividual() *Individual {
-	return &Individual{
-		fitness: 0,
-		genes:   make([]float64, GeneCount),
+	genes := make([]int, GeneCount)
+
+	src := rand.NewSource(time.Now().UnixNano())
+	x := rand.New(src).Int()
+
+	for i := 0; i < GeneCount; i++ {
+		genes[i] = x & 0x01
+		x >>= 1
 	}
+
+	return &Individual{0, genes}
 }
--- core/individual_test.go
+++ core/individual_test.go
@@ -1,28 +1,18 @@
 package core
 
 import (
-	"reflect"
 	"testing"
 )
 
 func Test_NewIndividual(t *testing.T) {
-	tests := []struct {
-		name   string
-		expect *Individual
-	}{
-		{
-			name:   "all good",
-			expect: &Individual{fitness: 0, genes: make([]float64, GeneCount)},
-		},
+	i := NewIndividual()
+
+	if i == nil {
+		t.Error("individual is nil")
+		return
 	}
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			i := NewIndividual()
-
-			if !reflect.DeepEqual(i, test.expect) {
-				t.Errorf("expected %v to be %v", i, test.expect)
-			}
-		})
+	if len(i.genes) != GeneCount {
+		t.Error("genes array length is wrong")
 	}
 }
